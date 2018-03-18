8433bab3afd0b4b28c3fe2072da8aaf8cef466e6
Angel M <angel@laux.es>
2018-03-19 00:31:09 +0100

    Fix routes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
-  exportPathMap: function() {
-    return {
-      '/': { page: '/' }
-    }
+  exportPathMap: () => {
+    const routes = {
+      '/': { page: '/' },
+      '/articles': { page: '/articles' },
+    };
+
+    const files = fs.readdirSync('./articles/');
+
+    // Prepare articles!
+    files.forEach((f) => {
+      routes[`/articles/${path.parse(f).name}`] = {
+        page: '/article',
+        query: { id: path.parse(f).name }
+      }
+    });
+
+    return routes;
   }
 }
