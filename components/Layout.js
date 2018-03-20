fbf62a1d1dc18ccc57ee95442775645d79320950
Angel M De Miguel <angel@bitnami.com>
2018-03-20 14:43:17 +0100

    Add the favicon

--- components/Layout.js
+++ components/Layout.js
@@ -32,6 +32,19 @@ class Layout extends React.Component {
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#8A2FFF" />
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/static/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/static/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/static/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/static/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/static/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/static/apple-touch-icon-152x152.png" />
+        <link rel="icon" type="image/png" href="/static/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/static/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/static/favicon-196x196.png" sizes="196x196" />
+        <meta name="application-name" content="Angelmm.rocks"/>
+        <meta name="msapplication-TileColor" content="#8A2FFF" />
+        <meta name="msapplication-TileImage" content="/static/mstile-144x144.png" />
+        <meta name="msapplication-square310x310logo" content="/static/mstile-310x310.png" />
       </Head>
       <div>
         { this.props.children }
