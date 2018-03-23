0a6d32826d4d5e8bb3d7991eb88ba4436951c26b
Angel M <angel@laux.es>
2018-03-23 23:11:04 +0100

    Add the mobile navigation

---
+++ components/Header/Header.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import Link from 'next/link';
+import Mobile from './Mobile';
+
+// Main header
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    }
+  }
+
+  render() {
+    return <header className={ `container ${this.state.open ? 'open' : ''}` }>
+      <div className="content">
+        <div className="logo">
+          <Link href="/">
+            <a>
+              <img src="/static/images/logo.svg" alt="Angelmm.rocks logo" />
+            </a>
+          </Link>
+        </div>
+        <button className="mobile" onClick={ () => this.setState({ open: !this.state.open })}>
+          <Mobile style={ { height: '2em' }}/>
+        </button>
+        <nav>
+          <Link href="/articles">
+            <a>Blog</a>
+          </Link>
+          <Link href="/">
+            <a>Projects</a>
+          </Link>
+          <Link href="/">
+            <a>Photography</a>
+          </Link>
+          <Link href="/">
+            <a>Videogames</a>
+          </Link>
+        </nav>
+      </div>
+      <style jsx>{`
+        .content {
+          align-items: center;
+          display: flex;
+          justify-content: space-between;
+          padding: 2rem 0;
+        }
+
+        a {
+          color: var(--c-purple-transparent);
+          font-size: 18px;
+          font-weight: bold;
+          margin-right: 1rem;
+          text-shadow: 2px 2px 0 var(--c-pink-transparent);
+          text-transform: uppercase;
+        }
+
+        nav a:last-child {
+          margin-right: 0;
+        }
+
+        .mobile {
+          border: none;
+          background: none;
+          cursor: pointer;
+          display: none;
+        }
+
+        @media screen and (max-width: 590px) {
+          .content {
+            position: relative;
+          }
+
+          .mobile {
+            display: block;
+            z-index: 2;
+          }
+
+          .logo {
+            z-index: 2;
+          }
+
+          nav {
+            background-color: rgba(255, 255, 255, .9);
+            display: none;
+            min-height: calc(100vh - 5rem);
+            top: 0;
+            padding-top: 5rem;
+            position: absolute;
+            width: 100%;
+            z-index: 1;
+          }
+
+          nav a {
+            display: block;
+            font-size: 2.5em;
+            text-align: center;
+            margin: 1rem 0;
+            opacity: 0;
+            will-change: transfrom;
+          }
+
+          .open nav {
+            display: block;
+            animation: fadein .15s ease-in;
+          }
+
+          .open nav a {
+            animation: fadein-links .4s cubic-bezier(0.77, 0, 0.175, 1) .15s forwards;
+          }
+
+          .open nav a:nth-child(2) {
+            animation-delay: .20s;
+          }
+
+          .open nav a:nth-child(3) {
+            animation-delay: .25s;
+          }
+
+          .open nav a:nth-child(4) {
+            animation-delay: .30s;
+          }
+        }
+
+        @keyframes fadein {
+          from {
+            opacity: 0;
+          }
+          to {
+            opacity: 1;
+          }
+        }
+
+        @keyframes fadein-links {
+          from {
+            opacity: 0;
+            transform: translateY(-15px);
+          }
+          50% {
+            opacity: .8;
+            transform: translateY(5px);
+          }
+          to {
+            opacity: 1;
+            transform: translateY(0);
+          }
+        }
+      `}</style>
+    </header>;
+  }
+};
+
+export default Header;
---
+++ components/Header/Mobile.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Mobile = props => (
+  <svg viewBox="0 0 160 73" {...props}>
+    <title>Mobile</title>
+    <g fill="none" fillRule="evenodd">
+      <g transform="translate(4 4)" fill="#F24592">
+        <rect y={44} width={156} height={25} rx={12.5} />
+        <path d="M4 28v-4H0v-4h8v4h12v-8H4v-4H0V4h4V0h20v4h4v4h-8V4H8v8h16v4h4v8h-4v4H4zm40 0V4h-8V0h24v4h-8v24h-8zm20 0V8h4V4h4V0h12v4h4v4h4v20h-8v-8H72v8h-8zm8-12h12V8h-4V4h-4v4h-4v8zm24 12V0h24v4h4v12h-8v4h4v4h4v4h-12v-4h-4v-4h-4v8h-8zm8-12h8v-4h4V4h-12v12zm36 12V4h-8V0h24v4h-8v24h-8z" />
+      </g>
+      <g fill="#8A2FFF">
+        <rect y={44} width={156} height={25} rx={12.5} />
+        <path d="M4 28v-4H0v-4h8v4h12v-8H4v-4H0V4h4V0h20v4h4v4h-8V4H8v8h16v4h4v8h-4v4H4zm40 0V4h-8V0h24v4h-8v24h-8zm20 0V8h4V4h4V0h12v4h4v4h4v20h-8v-8H72v8h-8zm8-12h12V8h-4V4h-4v4h-4v8zm24 12V0h24v4h4v12h-8v4h4v4h4v4h-12v-4h-4v-4h-4v8h-8zm8-12h8v-4h4V4h-12v12zm36 12V4h-8V0h24v4h-8v24h-8z" />
+      </g>
+    </g>
+  </svg>
+);
+
+export default Mobile;
+
