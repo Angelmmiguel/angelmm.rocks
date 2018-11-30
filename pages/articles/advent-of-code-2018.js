bb91a0e602387d7f84819b4b08f290a8310db87e
Angel M De Miguel <angel@bitnami.com>
2018-11-30 16:45:14 +0100

    Add the advent of code article

---
+++ pages/articles/advent-of-code-2018.js
@@ -0,0 +1,4 @@
+import Article from '../../components/Article';
+import Content, { meta } from '../../articles/advent-of-code-2018.mdx';
+
+export default () => <Article Content={ Content } meta={ meta } />;
