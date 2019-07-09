ec974c5a6317c664e165109ea13ed8eb7379f32f
Angel M <angel@laux.es>
2019-07-10 00:00:25 +0200

    Boilerplate for the new article

---
+++ pages/articles/minimal-routing-for-react-with-wouter.js
@@ -0,0 +1,4 @@
+import Article from '../../components/Article';
+import Content, { meta } from '../../articles/minimal-routing-for-react-with-wouter.mdx';
+
+export default () => <Article Content={ Content } meta={ meta } />;
