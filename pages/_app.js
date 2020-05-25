5cbaa0b0a8a1f1b100ea348ea4fb9b2f377066ed
Angel M De Miguel <dangel@vmware.com>
2020-05-25 09:11:19 +0200

    Revert "Add fathom stats (better privacy)"
    This reverts commit e8243510f979e91e0d8bd326cf22d664b3ed344c.

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import App from 'next/app';
-import Router from 'next/router';
-
-Router.events.on('routeChangeComplete', path => {
-  window.fathom('trackPageview', { path });
-})
-
-export default App;
